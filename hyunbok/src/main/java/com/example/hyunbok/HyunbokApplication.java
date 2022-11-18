b18284f50e1f7c6648719fa1583999fc6825336d
hyunboklee <gusqhrzkq@naver.com>
2022-11-18 13:56:38 +0900

    Feat : project 생성

---
+++ hyunbok/src/main/java/com/example/hyunbok/HyunbokApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyunbok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyunbokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyunbokApplication.class, args);
+    }
+
+}
